30f2adc4c0ff95c6d8c00028cf41c3878f815376
Chan Chhaya <it.chhaya@gmail.com>
2023-04-19 08:40:17 +0700

    first commit

---
+++ src/main/java/co/istad/thymeleaf/webapp/controller/ArticleController.java
@@ -0,0 +1,13 @@
+package co.istad.thymeleaf.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ArticleController {
+
+    @GetMapping("/article")
+    String viewArticle() {
+        return "pages/article";
+    }
+}
